56f5aa1c7aa002251d8405528e26c6892ffef722
FatimaYousef <49402825+FatimaYousef@users.noreply.github.com>
2023-11-10 16:43:52 +0300

    Add files via upload

---
+++ counter/app.js
@@ -0,0 +1,32 @@
+let count = 0;
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(btn) {
+  btn.addEventListener('click' , function(e){
+    const styles = e.currentTarget.classList;
+    if(styles.contains('decrease'))
+    {
+      count--;
+    }
+   else if (styles.contains('increase')) {
+       count++;
+       }
+    else {
+      count = 0;
+    }
+    if( count > 0)
+    {
+      value.style.color = 'green';
+    }
+    else if ( count < 0 )
+    {
+      value.style.color = 'red';
+    }
+    else {
+      value.style.color = 'black';
+    }
+    value.textContent = count;
+  })
+
+});
